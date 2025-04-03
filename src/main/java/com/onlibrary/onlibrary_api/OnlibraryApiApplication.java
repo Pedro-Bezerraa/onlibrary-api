d1896c6a7d679fe452ef8c606876d426891d3e18
Pedro-Bezerraa <pedro.henrique22012007@gmail.com>
2025-04-03 20:44:18 -0300

    Init commit: Estrutura base do pojeto

---
+++ src/main/java/com/onlibrary/onlibrary_api/OnlibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.onlibrary.onlibrary_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlibrary/onlibrary_api/OnlibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlibrary.onlibrary_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
