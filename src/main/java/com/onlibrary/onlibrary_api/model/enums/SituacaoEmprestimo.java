214b1cd6a3861a4d0a0c8de02cf6ac0e21b8a96e
Pedro-Bezerraa <pedro.henrique22012007@gmail.com>
2025-06-01 16:05:52 -0300

    fix: corrigido valores aceito no Request Body

--- src/main/java/com/onlibrary/onlibrary_api/model/enums/SituacaoEmprestimo.java
+++ src/main/java/com/onlibrary/onlibrary_api/model/enums/SituacaoEmprestimo.java
@@ -1,10 +1,27 @@
 package com.onlibrary.onlibrary_api.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.onlibrary.onlibrary_api.exception.BusinessException;
 
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum SituacaoEmprestimo {
     CONCLUIDO,
     PENDENTE,
-    CANCELADO
+    CANCELADO;
+
+    @JsonCreator
+    public static SituacaoEmprestimo fromString(String value) {
+        try {
+            return SituacaoEmprestimo.valueOf(value.trim().toUpperCase());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new BusinessException("Situação inválida. Use DISPONIVEL ou INDISPONIVEL.");
+        }
+    }
+
+    @JsonValue
+    public String getValue() {
+        return this.name();
+    }
 }
