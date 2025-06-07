57317479494629600d4b7309b266bd23b1d668f2
Pedro-Bezerraa <pedro.henrique22012007@gmail.com>
2025-06-07 18:16:25 -0300

    fix: arrumado entrada de dados do DTO

--- src/main/java/com/onlibrary/onlibrary_api/dto/reserva/AttReservaRequestDTO.java
+++ src/main/java/com/onlibrary/onlibrary_api/dto/reserva/AttReservaRequestDTO.java
@@ -1,10 +1,12 @@
 package com.onlibrary.onlibrary_api.dto.reserva;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AttReservaRequestDTO(
-        LocalDate dataRetirada,
-        UUID bibliotecarioId
+        @JsonProperty("data_retirada") LocalDate dataRetirada,
+        @JsonProperty("fk_id_bibliotecario") UUID bibliotecarioId
 ) {
 }
--- src/main/java/com/onlibrary/onlibrary_api/dto/reserva/ReservaRequestDTO.java
+++ src/main/java/com/onlibrary/onlibrary_api/dto/reserva/ReservaRequestDTO.java
@@ -1,16 +1,17 @@
 package com.onlibrary.onlibrary_api.dto.reserva;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.onlibrary.onlibrary_api.model.enums.TipoReserva;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 public record ReservaRequestDTO(
-        UUID bibliotecaId,
-        UUID usuarioId,
-        UUID bibliotecarioId,
-        UUID livroId,
-        TipoReserva tipoReserva,
-        BigDecimal quantidade
+        @JsonProperty("fk_id_biblioteca") UUID bibliotecaId,
+        @JsonProperty("fk_id_usuario") UUID usuarioId,
+        @JsonProperty("fk_id_bibliotecario") UUID bibliotecarioId,
+        @JsonProperty("fk_id_livro") UUID livroId,
+        TipoReserva tipo,
+        @JsonProperty("quantidade_total") BigDecimal quantidade
 ) {
 }
