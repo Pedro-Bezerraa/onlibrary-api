0624b589aa5d74a679bf3b269688d78369ad0f95
Pedro-Bezerraa <pedro.henrique22012007@gmail.com>
2025-06-23 20:54:19 -0300

    fix: corrigido resposta de view

--- src/main/java/com/onlibrary/onlibrary_api/dto/exemplar/ExemplarDependenciesDTO.java
+++ src/main/java/com/onlibrary/onlibrary_api/dto/exemplar/ExemplarDependenciesDTO.java
@@ -1,5 +1,6 @@
 package com.onlibrary.onlibrary_api.dto.exemplar;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.onlibrary.onlibrary_api.dto.LabelValueDTO;
 
 import java.util.List;
@@ -11,7 +12,7 @@ public record ExemplarDependenciesDTO(
         String setor,
         String prateleira,
         String estante,
-        List<LabelValueDTO> todos_os_livros_da_biblioteca
+        @JsonProperty("livros_biblioteca") List<LabelValueDTO> todos_os_livros_da_biblioteca
 ) {
     public record LabelValue<T>(String label, T value) {}
 
