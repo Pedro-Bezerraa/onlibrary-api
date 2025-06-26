6fb044bb061d0865d43f13d18902dbabce324ca8
Pedro-Bezerraa <pedro.henrique22012007@gmail.com>
2025-06-26 11:43:37 -0300

    fix: corrigido nome de capa

--- src/main/java/com/onlibrary/onlibrary_api/dto/livro/LivroRequestDTO.java
+++ src/main/java/com/onlibrary/onlibrary_api/dto/livro/LivroRequestDTO.java
@@ -1,10 +1,12 @@
 package com.onlibrary.onlibrary_api.dto.livro;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 import java.util.UUID;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record LivroRequestDTO(
         String isbn,
         String titulo,
